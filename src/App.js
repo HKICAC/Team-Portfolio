df2b6219a62e5ddde4514a25bbd38193b963a9eb
Edwin Kam <peterhi530@gmail.com>
2021-12-10 01:21:40 -0800

    added expereince but looks pretty ugly

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import Intro from './components/Intro/Intro.jsx';
 import About from './components/About/About.jsx';
+import Experience from './components/Experience/Experience.jsx';
 
 function App() {
   return (
     <div className="App">
       <Intro/>
       <About/>
+      <Experience/>
     </div>
   );
 }
---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Typical from 'react-typical'
+import "./experience.css";
+import Me from "../../img/apple.png";
+
+function Experience() {
+  return (
+    <div>
+      
+      <div className="experience">
+        <div className="header">
+        <h1>Experience</h1>
+        </div>
+        
+        <div className="experience-left">
+            <div className="experience-job">
+                <h2>Apple Software Engineering Intern</h2>
+                <p>2020-2021</p>
+            </div>
+            
+            
+        </div>
+          
+        <div className="experience-right">
+            <div className="experience-job">
+                <img className="experience-img" src={Me} alt=""/>
+            </div>
+          
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Experience;
